5c3b3a39ba12fcf7186fc7017d117fdb7f2e9dc3
Hardik <itsolutionstuff@gmail.com>
2022-02-09 08:38:00 +0530

    first commit 2

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 9 CRUD Application - ItSolutionStuff.com</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+        body{
+            padding: 50px;
+            background: #f2f5f2;
+        }
+    </style>
+</head>
+<body>
+  
+<div class="container">
+    @yield('content')
+</div>
+   
+</body>
+</html>
